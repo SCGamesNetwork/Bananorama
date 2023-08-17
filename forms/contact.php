df7c4aca228775b8e24f278695dbfb1ce70d6f4b
SCGamesNetwork <100749275+SCGamesNetwork@users.noreply.github.com>
2023-08-18 00:19:27 +0700

    Delete contact.php

--- forms/contact.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $nomor = '6285814083661';
-  $template = "Name: " . $_POST["name"] .
-  "\nSubject: " . $_POST["subject"] . 
-  "\n\nMessage: \n" . $_POST["message"];
-
-  header("Location: https://wa.me/" . $nomor . "?text=" . urlencode($template));
-?>
