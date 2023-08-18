43f4962babf944c522177b5337aa49039e480d1f
SCGamesNetwork <100749275+SCGamesNetwork@users.noreply.github.com>
2023-08-18 14:27:16 +0700

    Update submit.js

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -1,14 +1,15 @@
 function submitForm(event) {
   event.preventDefault();
+ document.getElementById("error-msg").style.display = "none";
+ document.getElementById("sent-msg").style.display = "none";
  
   var name = document.getElementById("name").value;
   var subject = document.getElementById("subject").value;
   var message = document.getElementById("message").value;
   
   if (name === "" || subject === "" || message === "") {
-    var errorMsg = document.getElementById("error-msg");
-    errorMsg.textContent = "Please fill in all the required fields.";
-    errorMsg.style.display = "block";
+    document.getElementById("error-msg").textContent = "Please fill in all the required fields.";
+    document.getElementById("error-msg").style.display = "block";
     return;
   }
 
